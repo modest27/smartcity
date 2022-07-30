e1794fc63bf4c068328fade65102a9d09b9f9082
modest_ <1342994266@qq.com>
2022-07-30 12:43:39 +0800

    创建项目和支持glsl语法

---
+++ src/components/Scene.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="scene"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+// 场景div
+let sceneDiv = ref(null)
+
+onMounted(()=>{
+  console.log(sceneDiv);
+})
+
+</script>
+
+<style>
+.scene{
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>测试</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import testVertex from '@/shader/test/vertex.glsl'
+console.log(testVertex);
 
 export default {
   name: 'HomeView',
